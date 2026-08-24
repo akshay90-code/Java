a6f8f009139f5267f80e4c178d1ef6fa54b00aa0
akshay90-code <akshay9036t@gmail.com>
2026-08-24 15:31:34 +0530

    Add files via upload

---
+++ array2D.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class array2D {
+    public static void main(String[]args){
+        int a[][]=new int[3][3];
+                Scanner sc=new Scanner(System.in);
+
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+                        a[i][j]=sc.nextInt();}
+
+            }
+             for(int k=0;k<3;k++){
+            for(int j=0;j<3;j++){
+                 System.out.printf("%d\t",a[k][j]);
+}
+    System.out.println();
+
+        }
+        sc.close();
+        
+    
+    }}
+    
+
