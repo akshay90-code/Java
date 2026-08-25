e689c6278b5289ec0c5925b7be1f301e9f0d51ee
akshay90-code <akshay9036t@gmail.com>
2026-08-25 09:24:52 +0530

    Add files via upload

---
+++ string.java
@@ -0,0 +1,17 @@
+public class string {
+    public static void main(String[]args){
+        String t="Hello Java";
+        System.out.println(t.length());
+        System.out.println(t.charAt(0));
+        System.out.println(t.substring(6));
+        System.out.println(t.toUpperCase());
+        System.out.println(t.toLowerCase());
+        System.out.println(t.contains("Java"));
+        System.out.println(t.startsWith("Hello"));
+        System.out.println(t.endsWith("Java"));
+        System.out.println(t.indexOf("J"));
+
+
+    }
+    
+}
